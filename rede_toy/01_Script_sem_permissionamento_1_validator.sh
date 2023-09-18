7105387c6f804159530277ff3b6df90436380304
R4wd0G <wpa.sys@hotmail.com>
2023-09-18 12:48:22 -0300

    .

--- rede_toy/01_Script_sem_permissionamento_1_validator.sh
+++ rede_toy/01_Script_sem_permissionamento_1_validator.sh
@@ -11,7 +11,7 @@ echo "Dica:"
 echo "Utilize portas que não estão em uso"
 sleep 4
 
-git clone https://github.com/RBBNet/start-network.git
+curl -#SL https://github.com/RBBNet/start-network/releases/download/v0.4.0-permv1/start-network.tar.gz | tar xz
 mv start-network $projectname
 cd $projectname
 # cria os nós especificados.
